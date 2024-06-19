30d9f8fadc9058ed0fc5125c9743e8766b11b346
viksamiles77 <mileskiviktor77@gmail.com>
2024-06-19 22:13:08 +0200

    Pizza App part 2

--- PizzaApp/PizzaApp/Controllers/PizzaController.cs
+++ PizzaApp/PizzaApp/Controllers/PizzaController.cs
@@ -1,32 +1,82 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Services.Implementation;
 using Services.Interfaces;
+using ViewModels;
 
 namespace PizzaApp.Controllers
 {
     public class PizzaController : Controller
     {
-        private IPizzaService _pizzaservice;
+        private IPizzaService _pizzaService;
 
         public PizzaController()
         {
-            _pizzaservice = new PizzaService();
+            _pizzaService = new PizzaService();
         }
+
         public IActionResult Index()
         {
-            var items = _pizzaservice.GetAll();
+            var items = _pizzaService.GetAll();
             return View(items);
         }
 
         public IActionResult Details(int id)
         {
-            var item = _pizzaservice.GetDetails(id);
+            var item = _pizzaService.GetDetails(id);
             return View(item);
         }
 
-        public IActionResult SearchByName(string name)
+        [HttpPost]
+        public IActionResult SearchByName([FromForm] FilterViewModel filterModel)
         {
-            return View();
+            var items = _pizzaService.SearchByName(filterModel.Name);
+            return View("Index", items);
+        }
+
+        public IActionResult Create()
+        {
+            var pizza = new PizzaViewModel();
+            return View(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] PizzaViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _pizzaService.Create(model);
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var pizza = _pizzaService.GetDetails(id);
+            return View(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromForm] PizzaViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _pizzaService.Update(model);
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _pizzaService.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
